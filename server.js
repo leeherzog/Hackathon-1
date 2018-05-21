9e88c77302196b5e12b967bace9df4a48d6f7bbf
Ronit Herzog <lee.herzog5@gmail.com>
2018-05-21 14:55:52 +0300

    mongo

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get('/likes/:id', (req, res) => {
 });
 
 
-app.listen(SERVER_PORT, "0.0.0.0" ,  () => {
+app.listen(process.env.PORT || '8080') ,  () => {
   console.log("Server started on port " + SERVER_PORT);
 });
 
